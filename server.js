a8734e3df9184c1d93395ccf9d3a61b046d0b709
Silvio Sol <sadwick1989@gmail.com>
2022-03-11 17:58:43 -0500

    set up auth routes and controller

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ import express from 'express';
 
 import { mongoConnect } from './db/connect.js';
 
+import authRouter from './routes/authRoutes.js';
+
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome!');
 })
+
+app.use('/api/v1/auth', authRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
