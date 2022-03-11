6273541b14bda7c96be711b10e253f2231e36e49
Silvio Sol <sadwick1989@gmail.com>
2022-03-11 16:43:50 -0500

    created basic project structure

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Server running...');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+  console.log(`Server is listening on port ${PORT}...`);
+});
