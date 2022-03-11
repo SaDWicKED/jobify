ebe45bea8ea0fad05682babec7379f26b2c1a908
Silvio Sol <sadwick1989@gmail.com>
2022-03-11 17:03:45 -0500

    added error-handler and not-found middlewares

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
+import dotenv from 'dotenv';
 
+dotenv.config()
 const app = express();
 
 app.get('/', (req, res) => {
+  throw new Error('error');
   res.send('Welcome!');
 })
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
