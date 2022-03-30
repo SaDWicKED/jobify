8ad6dedd1b65e1f1ec1f61254318528e47288216
Silvio Sol <sadwick1989@gmail.com>
2022-03-30 17:29:18 -0400

    removed unnecessary logs and dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,7 @@ import {
 import { 
   BrowserRouter, 
   Routes, 
-  Route, 
-  Link 
+  Route,  
 } from 'react-router-dom'
 import {
   AllJobs,
--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,6 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { userId: payload.userId};
-    console.log(payload);
     next();
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid');
