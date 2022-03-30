151837f25adac82f8f0d40c390f3816fd0045ff7
Silvio Sol <sadwick1989@gmail.com>
2022-03-30 18:12:20 -0400

    prepared app for production

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,10 +6,18 @@ import {
 } from '../controllers/authController.js';
 import authenticateUser from '../middleware/auth.js';
 
+import rateLimiter from 'express-rate-limit';
+
 const authRouter = express.Router();
 
-authRouter.route('/register').post(register)
-authRouter.route('/login').post(login)
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: { msg:'Too many requests from this IP, please try again after 15 minutes' },
+})
+
+authRouter.route('/register').post(apiLimiter, register)
+authRouter.route('/login').post(apiLimiter, login)
 authRouter.route('/updateUser').patch(authenticateUser, updateUser)
 
 export default authRouter;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import express from 'express';
 const app = express();
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+
 import 'express-async-errors';
 import morgan from 'morgan';
 
@@ -18,18 +26,22 @@ if(process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './client/build')))
+
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
-app.get('/', (req, res) => {
-  res.json({msg:'Welcome!'});
-});
-
-app.get('/api/v1', (req, res) => {
-  res.json({msg:'API!'});
-});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
